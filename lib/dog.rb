7bc734487c6d44e301e96de482eb27ae64195f38
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-18 23:30:59 -0500

    commit tree 73e4a924d39b00399a6b9a1180b55f9c392a2a65

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog
+    attr_accessor :name 
+    @@all = []
+    @@names = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+        @@names << name
+        save
+    end
+
+    def self.all
+        @@all.uniq
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        huh = @@names.uniq
+        ugg = huh.delete_if{|x| x == "Snoopy"}
+        puts ugg
+
+    end
+
+    def save
+        @@all << self
+    end
+
+end
